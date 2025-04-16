cae1f5d5012e0ff7be8940a87a32bb666b9710a5
techinstance <technologyinstance@gmail.com>
2025-04-16 13:07:29 +0530

    Message

--- app/Http/Controllers/Hotel/CouponRedeemController.php
+++ app/Http/Controllers/Hotel/CouponRedeemController.php
@@ -124,8 +124,7 @@ class CouponRedeemController extends Controller
 	{
 		$check = $this->check($request, 'cr-reedem', 'ajax');
 		if ($check) {
-			$user = OrderDetails::where('id', $id)->update(['status'=>$status]);
- 		
+			$user = OrderDetails::where('id', $id)->update(['status'=>'Redeem']);
 			$response['success'] = true;
 			$response['message'] = 'Status Changed SuccessFully';
 		}else{
